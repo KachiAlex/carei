e023a4473ebb0caff7cc4cdfe619b997eaa7cfdd
Kachi Alex <opd.livmind@gmail.com>
2026-06-04 06:36:41 +0100

    fix: move shared db module to api/_utils/ with includeFiles config

--- api/sos.ts
+++ api/sos.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { sql } from './db'
+import { sql } from './_utils/db'
 import { broadcast } from './events'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
